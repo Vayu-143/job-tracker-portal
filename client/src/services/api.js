59a455a84c8297c8cb3b22c827a419311586e868
Vayu-143 <vayumishra143@gmail.com>
2026-05-26 16:53:03 +0530

    Added production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,23 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-API.interceptors.request.use(
-  (req) => {
-    const user = JSON.parse(
-      localStorage.getItem("user")
-    );
+API.interceptors.request.use((req) => {
+  const user = JSON.parse(localStorage.getItem("user"));
 
-    if (user?.token) {
-      req.headers.Authorization =
-        `Bearer ${user.token}`;
-    }
-
-    return req;
+  if (user?.token) {
+    req.headers.Authorization = `Bearer ${user.token}`;
   }
-);
+
+  return req;
+});
 
 export default API;
